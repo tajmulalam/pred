52c196a6414a4ea275fda07ae61ec82603ff1fe8
Md Tajmul Alam <tajmul.alam@multipletrading.net>
2018-10-17 17:39:49 +0600

    login functional

--- Predicate/predicate/predicateapp/views.py
+++ Predicate/predicate/predicateapp/views.py
@@ -1,14 +1,26 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-
+from django.views.decorators.csrf import csrf_exempt
+from .utils import Utils
+import json
+from django.http import JsonResponse
 
 def index(request):
     return render_to_response('index.html')
 
-
+@csrf_exempt
 def login(request):
-    return render_to_response('login.html')
+	if request.method=='GET':
+		return render_to_response('login.html')    	
+	elif request.method == 'POST':
+		utils=Utils()
+		data = json.loads(request.body)
+		print(data['username'])
+		validUser=utils.checkCredentials(data['username'],data['password'])
+		return HttpResponse(json.dumps({'user':validUser}),content_type='application/json')
+	else:
+		return render_to_response('login.html')
 
 
 def addDataset(request, challenge_id):
